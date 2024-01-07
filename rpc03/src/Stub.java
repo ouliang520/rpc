5a22a195265e58b956f484a6e7332ab747e130a5
ouzhenjie <ouzhenjie@zwcad.com>
2024-01-07 22:48:40 +0800

    rpc03

---
+++ rpc03/src/Stub.java
@@ -0,0 +1,38 @@
+import com.ouliang.common.IUserService;
+import com.ouliang.common.User;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.net.Socket;
+
+// 代理,隐藏细节
+public class Stub {
+    public static IUserService getStub() {
+        InvocationHandler h = (proxy, method, args) -> {
+            Socket s = new Socket("localhost", 8888);
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            DataOutputStream dos = new DataOutputStream(baos);
+            dos.writeInt(123);
+
+            s.getOutputStream().write(baos.toByteArray());
+            s.getOutputStream().flush();
+
+            DataInputStream dis = new DataInputStream(s.getInputStream());
+            int id = dis.readInt();
+            String name = dis.readUTF();
+            User user = new User(id, name);
+
+            dos.close();
+            s.close();
+            return user;
+        };
+
+        Object o = Proxy.newProxyInstance(IUserService.class.getClassLoader(), new Class[]{IUserService.class}, h);
+        return (IUserService) o;
+    }
+}
